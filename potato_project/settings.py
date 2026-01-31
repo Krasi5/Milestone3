5680350c495eb58e7cdabe4183d6d8a5880435ab
Krasi5 <krasi_todorova@yahoo.com>
2026-01-31 18:37:22 +0000

    second beginning

--- potato_project/settings.py
+++ potato_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-leg&#x64%3ngs(ib_of-x%9q9vhtp6md)zqlhx_3z@re+ft+q)'
+SECRET_KEY = 'django-insecure-))i)eho#7x7&f%c&#o3e0k#pssalf+zvr8toz)tv4ea$b-vq$_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
