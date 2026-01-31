9cd99035f9c478fe962a83efe0d463ab6145c513
Krasi5 <krasi_todorova@yahoo.com>
2026-01-31 17:41:56 +0000

    spelling mistake correction

--- potato_project/urls.py
+++ potato_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from restourant import views as index_views
+from restaurant import views as index_views
 
 urlpatterns = [
     path('', index_views.index, name='index'),
