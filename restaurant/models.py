7abda2822c6a0272b3493d60e23a70200e8b296d
Krasimira Todorova <krasi_todorova@yahoo.com>
2026-02-24 23:32:10 +0000

    migration fix

--- restaurant/models.py
+++ restaurant/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="restaurant_posts",
+        User, on_delete=models.CASCADE, default=0, related_name="restaurant_posts",
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
